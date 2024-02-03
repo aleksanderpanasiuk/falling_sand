dc1ad204ca0f2dd2ce0a868bd8831184f7838bdf
aleksanderpanasiuk <olekp249@gmail.com>
2024-02-03 21:45:42 +0100

    Implemented base class for different cell types

---
+++ cellType.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class CellType:
+    def __init__(self, screen, screen_position: tuple, grid_position: tuple, size: int) -> None:
+        self._screen = screen
+        self._screen_position = screen_position
+        self._grid_position = grid_position
+        self._size = size
+
+        self._color = (255, 255, 255)
+
+
+    def events(self) -> None:
+        pass
+
+
+    def draw(self) -> None:
+        pygame.draw.rect(
+            self._screen, self._color,
+            pygame.Rect(
+                self._screen_position[0], self._screen_position[0],
+                self._size, self._size
+            )
+        )

b432380a42be7655a8713c514ef75fa167baea2a
aleksanderpanasiuk <olekp249@gmail.com>
2024-02-06 12:51:38 +0100

    Water now works correctly on flat surfaces

--- Water.py
+++ Water.py
@@ -7,6 +7,7 @@ class Water(CellType):
         super().__init__(screen, screen_position, grid_position, size)
 
         self._color = (0, 102, 255)
+        self._last_random_direction = -1
 
     def simulate(self, grid):
         height = len(grid)
@@ -27,9 +28,18 @@ class Water(CellType):
             fall_left = False
 
             if can_move_left and can_move_right:
-                fall_left = random.choice([True, False])
+                if self._last_random_direction == -1:
+                    fall_left = random.choice([True, False])
+                    self._last_random_direction = fall_left
+                else:
+                    fall_left = self._last_random_direction
+
             elif can_move_left:
                 fall_left = True
+                self._last_random_direction = -1
+            elif can_move_right:
+                self._last_random_direction = -1
+
             elif not can_move_left and not can_move_right:
                 return
 
